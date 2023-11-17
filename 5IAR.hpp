69268a10e85c2732ec7289606f5eac1f5979c35d
tyler <tyler.kelsey17@gmail.com>
2023-11-17 15:44:44 +0100

    added stats tracker

--- 5IAR.hpp
+++ 5IAR.hpp
@@ -6,3 +6,16 @@ void runGame();
 void setupGame();
 
 void printField(vector<int> field);
+
+class StatsTracker {
+    StatsTracker() : my_bot_wins(0), evil_bot_wins(0), my_bot_illegal_moves(0), evil_bot_illegal_moves(0){};
+
+    float myBotWR() {
+        return my_bot_wins / (my_bot_wins + evil_bot_wins) / 100;
+    }
+
+    int my_bot_wins;
+    int evil_bot_wins;
+    int my_bot_illegal_moves;
+    int evil_bot_illegal_moves;
+}
