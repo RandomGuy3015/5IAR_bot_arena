f03c197eb1430cdd135bd7a0c149d8f1bbd9b04c
Kai <kai.malaka@uranus.uni-freiburg.de>
2023-11-22 11:01:56 +0100

    added << operator for stats

--- 5IAR.cpp
+++ 5IAR.cpp
@@ -2,6 +2,7 @@
 #include "myBot.hpp"
 #include "evilBot.hpp"
 #include "board.hpp"
+
 #include <vector>
 #include <cstdio>
 #include <chrono>
@@ -19,11 +20,11 @@ void runGame() {
     board = Board();
 
 
-    int time_to_play = 1;
+    int time_to_play = -1;
     float time_per_turn = 0.001;
 
     unsigned long long start_time = time();
-    unsigned long long end_time = start_time + 10000;
+    unsigned long long end_time = start_time + time_to_play;
 
     int currentWinState = 0;
 
@@ -34,6 +35,7 @@ void runGame() {
             int my_bot_move = myBot.nextMove(board);
             if (!board.isLegal(my_bot_move)) {
                 printf("Illegal move: MyBot\n");
+                statsTracker.incIllegal(1);
                 currentWinState = 2;
                 break;
             }
@@ -48,22 +50,37 @@ void runGame() {
             if (!board.isLegal(evil_bot_move)) {
                 printf("Illegal move: EvilBot\n");
                 currentWinState = 1;
+                statsTracker.incIllegal(2);
                 break;
             }
             board.makeMove(evil_bot_move, 2);
             board.print();
             currentWinState = board.isMate();
         }
-        printf("Somebody won! Yay!");
+        printf("Player %d won! Yay!\n", currentWinState);
         statsTracker.incWin(currentWinState);
         
         // reset
         board.reset();
         currentWinState = 0;
     }
+    statsTracker.evil_bot_wins = 13;
+    statsTracker.my_bot_wins = 28;
+    cout << statsTracker;
 }
 
 unsigned long long time() {
   using namespace std::chrono;
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+ostream& operator<<(ostream& os, const StatsTracker &st){
+    os << fixed << setprecision(2);
+    os << "################### STATS ###################" <<                                  "\n";
+    os << "               "<< "MyBot"                 <<" \t EvilBot    "<<                   "\n"; 
+    os << "         wins: "<< st.my_bot_wins          <<" \t "<< st.evil_bot_wins <<          "\n";
+    os << "     win rate: "<< st.myBotWR() * 100      <<"%\t "<< (1 - st.myBotWR()) * 100 << "%\n";
+    os << "illegal moves: "<< st.my_bot_illegal_moves <<" \t "<< st.evil_bot_illegal_moves << "\n";
+    os << "#############################################"  <<                                 "\n";
+    return os;
 }
--- 5IAR.hpp
+++ 5IAR.hpp
@@ -1,5 +1,12 @@
+#ifndef FIAR_H
+#define FIAR_H
+
 #include <vector>
+#include <iostream>
+#include <iomanip>
+
 #include "board.hpp"
+
 using namespace std;
 
 void runGame();
@@ -10,17 +17,19 @@ class StatsTracker {
     public:
         StatsTracker() : my_bot_wins(0), evil_bot_wins(0), my_bot_illegal_moves(0), evil_bot_illegal_moves(0){};
 
-        float myBotWR() {
-            return my_bot_wins / (my_bot_wins + evil_bot_wins) / 100;
+        float myBotWR() const {
+            if (my_bot_wins + evil_bot_wins == 0){return 0;}
+            return (float) my_bot_wins / (float) (my_bot_wins + evil_bot_wins);
         }
 
-        void incWin(int win_value) {
-            if (win_value == 1) {
-                my_bot_wins++;
-            }
-            else if (win_value == 2) {
-                evil_bot_wins++;
-            }
+        void incWin(int player) {
+            if (player == 1) {my_bot_wins++;}
+            else if (player == 2) {evil_bot_wins++;}
+        }
+
+        void incIllegal(int player){
+            if (player == 1) {my_bot_illegal_moves++;}
+            else if (player == 2) {evil_bot_illegal_moves++;}
         }
 
         int my_bot_wins;
@@ -28,3 +37,7 @@ class StatsTracker {
         int my_bot_illegal_moves;
         int evil_bot_illegal_moves;
 };
+
+ostream& operator<<(ostream& os, const StatsTracker &st);
+
+#endif // FIAR_H
