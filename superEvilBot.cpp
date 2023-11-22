7467014360aa67e449e83d24e48c1c39ad2484c4
Kai <kai.malaka@uranus.uni-freiburg.de>
2023-11-22 18:17:12 +0100

    fixed 5000% win rate and randomised superEvilBot

--- superEvilBot.cpp
+++ superEvilBot.cpp
@@ -2,14 +2,15 @@
 #include "board.hpp"
 #include <map>
 #include <cstdio>
+#include <random>
 
 int SuperEvilBot::nextMove(Board board) {
     // ################ HERE GOES YOUR CODE #####################
-    if(board.moveCount == 0){
-        return board.getMoveID(10, 10);
-    }
     vector<int> moves = board.getLegalMoves();
-    int best_move = moves[moves.size() - 1];
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(0, moves.size() - 1); // define the range
+    int best_move = moves[distr(gen)];
     int best_score = 0;
     for (int move : moves){
         float sum = 0;
